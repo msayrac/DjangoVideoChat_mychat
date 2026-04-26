47bcb3fa9b2ba37a72f0d7a773cff15e7ee7613b
msayracode <msayracode@gmail.com>
2026-04-26 14:17:10 +0300

    style completed and agora account created

---
+++ static/js/streams.js
@@ -0,0 +1,9 @@
+
+const APP_ID = 'b7f67d03d0ce449e9970fc8e97cb6074'
+
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYAhuKK0MspDatyvE0mfPxJq5Prd9VS/bp0Rv0W57L/6xTF+BIck8zcw8xcA4xSA51cTEMtXS0twgLdki1dI8OcnMwNyE8fPbzIZARoYnk06xMjJAIIjPwpCbmJnHwAAADpAfgg=='
+
+
+console.log('Stream.js conntected')
+
